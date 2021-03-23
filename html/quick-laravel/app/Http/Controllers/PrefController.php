e2787273d39c8411b39038fe0310001ac6210cc2
Yuzunoha <studysteadystandby@gmail.com>
2021-03-23 22:11:43 +0900

    pref index です。

--- html/quick-laravel/app/Http/Controllers/PrefController.php
+++ html/quick-laravel/app/Http/Controllers/PrefController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PrefController extends Controller
 {
-    //
+    function index(Request $request)
+    {
+        return 'pref index です。';
+    }
 }
--- html/quick-laravel/routes/api.php
+++ html/quick-laravel/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', 'TestController@index');
+Route::get('/pref', 'PrefController@index');
