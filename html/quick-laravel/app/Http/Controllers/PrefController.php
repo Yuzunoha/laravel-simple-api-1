66657e3db1c43422766132e3ad21658c73e1b094
Yuzunoha <studysteadystandby@gmail.com>
2021-03-25 19:47:45 +0900

    ランダムな都道府県を返却する

--- html/quick-laravel/app/Http/Controllers/PrefController.php
+++ html/quick-laravel/app/Http/Controllers/PrefController.php
@@ -13,9 +13,15 @@ class PrefController extends Controller
         return $this->responseJsonUnescapedUnicode($data);
     }
 
+    /**
+     * random_int(2, 5) は値2と5をそれぞれ取り得る。
+     */
     function randomOne(Request $request)
     {
-        $data = 'random oneです';
+        $all = DB::select("SELECT * FROM kvs1");
+        $idxMax = count($all) - 1;
+        $idxRandom = random_int(0, $idxMax);
+        $data = $all[$idxRandom];
         return $this->responseJsonUnescapedUnicode($data);
     }
 }
